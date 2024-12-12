5e958da6bbf57d28e3855560cb62c10261a2e0f9
Alex <alex-xp123@hotmail.com>
2024-12-12 15:04:55 -0300

    First commit

---
+++ src/test/java/br/com/foods/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foods.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
