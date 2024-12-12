6dfbd1e7dca565b01b5e2d62b4a111bf751f4771
Alex <alex-xp123@hotmail.com>
2024-12-12 16:29:21 -0300

    Criado operacoes de crud

---
+++ src/main/java/br/com/foods/pagamentos/model/Status.java
@@ -0,0 +1,7 @@
+package br.com.foods.pagamentos.model;
+
+public enum Status {
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
---
+++ src/main/java/br/com/foods/pagamentos/repository/PagamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.foods.pagamentos.repository;
+
+import br.com.foods.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+}
